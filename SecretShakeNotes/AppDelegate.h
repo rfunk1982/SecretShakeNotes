c30de9708acf9d16ce8bdd9edd61ffea3a9fd579
Ricardo Funk <argentoenusa@hotmail.com>
2012-12-03 06:24:28 -0500

    Initial Commit

---
+++ SecretShakeNotes/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SecretShakeNotes
+//
+//  Created by Ricardo Funk on 12/3/12.
+//  Copyright (c) 2012 Ricardo Funk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SecretShakeNotes/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SecretShakeNotes
+//
+//  Created by Ricardo Funk on 12/3/12.
+//  Copyright (c) 2012 Ricardo Funk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
